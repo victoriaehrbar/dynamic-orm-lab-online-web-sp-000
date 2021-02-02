84ec990c92841f7d5285d9ad086a26a19527e354
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-02 02:06:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,6 +40,12 @@ class InteractiveRecord
     values.join(", ")
   end
   
+   def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+  
   
   def self.find_by_name(name)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
