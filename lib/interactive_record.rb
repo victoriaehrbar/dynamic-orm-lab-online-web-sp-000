be94ad0b4c4d303986259324842cb1049324f18a
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-02 02:06:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,7 +47,7 @@ class InteractiveRecord
   end
   
   
-  def self.find_by_name(name)
+def self.find_by_name(name)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
   DB[:conn].execute(sql, name)
 end
