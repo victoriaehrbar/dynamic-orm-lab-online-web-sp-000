2e18da0f14a94ef30e34edff127ba0cbfb3c5b22
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-02 02:02:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,26 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def self.table_name
+    "#{self.to_s.downcase}s"
+  end
+
+  def self.column_names
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+  
+    def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
   
 end
